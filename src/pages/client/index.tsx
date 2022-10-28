de63160f1ce82041a89d4a428c06457bcafde8a2
Ewerton Barbosa <ewertonb@live.com>
2022-10-28 01:03:34 -0300

    deploy

--- src/pages/client/index.tsx
+++ src/pages/client/index.tsx
@@ -1,9 +1,17 @@
 import styles from './styles.module.scss'
+import { toast } from 'react-toastify'
+import { FormEvent } from 'react'
 import Head from 'next/head'
 import { Header } from '../../components/ui/Header'
 import { Input } from '../../components/ui/Input'
 
 export default function Client() {
+
+  async function handle(event: FormEvent){
+    event.preventDefault();
+    
+    toast.info('Funcionalidade em desenvolvimento...')
+  }
   return (
     <>
       <Head>
@@ -19,21 +27,25 @@ export default function Client() {
           </h1>
 
           <form className={styles.form}>
-            <Input placeholder='Nome completo'/>
-            <Input placeholder='CPF'/>
-            <Input placeholder='Sexo'/>
-            <Input placeholder='Nacionalidade'/>
-            <Input placeholder='Data de Nascimento'/>
-            <Input placeholder='Contato Principal'/>
-            <Input placeholder='Secundário (Opcional)'/>
-            <Input placeholder='Email'/>
-            <Input placeholder='Empresa'/>
-            <Input placeholder='Cargo'/>
-            <Input placeholder='Status Ex: Ativo ou Inativo'/>
+            <Input placeholder='Nome completo' />
+            <Input placeholder='CPF' />
+            <Input placeholder='Sexo' />
+            <Input placeholder='Nacionalidade' />
+            <Input placeholder='Data de Nascimento' />
+            <Input placeholder='Contato Principal' />
+            <Input placeholder='Secundário (Opcional)' />
+            <Input placeholder='Email' />
+            <Input placeholder='Empresa' />
+            <Input placeholder='Cargo' />
+            <Input placeholder='Status Ex: Ativo ou Inativo' />
 
             <button
               className={styles.buttonAdd}
-              type='submit'>Cadastrar</button>
+              type='submit'
+              onClick={handle}
+            >
+              Cadastrar
+            </button>
           </form>
         </main>
       </div>
